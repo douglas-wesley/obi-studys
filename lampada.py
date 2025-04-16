6b481423dbef5c8c5caef8442fc7818ae35116b7
douglas-wesley <douglaswesley0407@gmail.com>
2025-04-16 16:16:13 -0300

    Corrigindo erro no lampada.py

--- lampada.py
+++ lampada.py
@@ -2,12 +2,13 @@ def lam(num):
     l1 = False
     l2 = False
 
-    for n in range(num):
+    for _ in range(num):
         val = int(input(""))
         if val == 1:
             l1 = not l1
         elif val == 2:
             l2 = not l2
+            l1 = not l1
 
     print("1" if l1 else "0")
     print("1" if l2 else "0")
